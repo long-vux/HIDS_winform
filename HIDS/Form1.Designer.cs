249dc08737e5c1d13f10123a9ea9d2f43cc9d3bd
long-vux <hoanglongvu233@gmail.com>
2024-12-26 17:30:14 +0700

    Add project files.

---
+++ HIDS/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace HIDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.listBoxLogs = new System.Windows.Forms.ListBox();
+            this.lblCpuUsage = new System.Windows.Forms.Label();
+            this.lblAvailableRam = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(288, 115);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(301, 22);
+            this.txtDirectory.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập đường dẫn";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(624, 102);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 49);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(624, 169);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 49);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Dừng lại";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // listBoxLogs
+            // 
+            this.listBoxLogs.FormattingEnabled = true;
+            this.listBoxLogs.ItemHeight = 16;
+            this.listBoxLogs.Location = new System.Drawing.Point(178, 327);
+            this.listBoxLogs.Name = "listBoxLogs";
+            this.listBoxLogs.Size = new System.Drawing.Size(571, 196);
+            this.listBoxLogs.TabIndex = 5;
+            // 
+            // lblCpuUsage
+            // 
+            this.lblCpuUsage.AutoSize = true;
+            this.lblCpuUsage.Location = new System.Drawing.Point(174, 257);
+            this.lblCpuUsage.Name = "lblCpuUsage";
+            this.lblCpuUsage.Size = new System.Drawing.Size(35, 16);
+            this.lblCpuUsage.TabIndex = 6;
+            this.lblCpuUsage.Text = "CPU";
+            // 
+            // lblAvailableRam
+            // 
+            this.lblAvailableRam.AutoSize = true;
+            this.lblAvailableRam.Location = new System.Drawing.Point(174, 289);
+            this.lblAvailableRam.Name = "lblAvailableRam";
+            this.lblAvailableRam.Size = new System.Drawing.Size(37, 16);
+            this.lblAvailableRam.TabIndex = 7;
+            this.lblAvailableRam.Text = "RAM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(282, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(396, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "HỆ THỐNG GIÁM SÁT HIDS";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(175, 222);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(67, 16);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Trạng thái";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "hoặc chọn file";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(288, 151);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(140, 33);
+            this.btnBrowse.TabIndex = 11;
+            this.btnBrowse.Text = "Chọn file";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 571);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAvailableRam);
+            this.Controls.Add(this.lblCpuUsage);
+            this.Controls.Add(this.listBoxLogs);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDirectory);
+            this.Name = "Form1";
+            this.Text = "HIDS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox listBoxLogs;
+        private System.Windows.Forms.Label lblCpuUsage;
+        private System.Windows.Forms.Label lblAvailableRam;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ HIDS/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace HIDS
+{
+    public partial class Form1 : Form
+    {
+
+        private FileSystemWatcher watcher;
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+        private Timer systemMetricsTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePerformanceCounters();
+            InitializeTimer();
+            btnStop.Enabled = false;
+        }
+
+        private void InitializePerformanceCounters()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        private void InitializeTimer()
+        {
+            systemMetricsTimer = new Timer();
+            systemMetricsTimer.Interval = 1000; // 1 giây
+            systemMetricsTimer.Tick += SystemMetricsTimer_Tick;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string path = txtDirectory.Text;
+
+            if (Directory.Exists(path))
+            {
+                watcher = new FileSystemWatcher();
+                watcher.Path = path;
+                watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+                watcher.Filter = "*.*";
+
+                watcher.Changed += OnChanged;
+                watcher.Created += OnChanged;
+                watcher.Deleted += OnChanged;
+                watcher.Renamed += OnRenamed;
+
+                watcher.EnableRaisingEvents = true;
+                lblStatus.Text = "Đang giám sát: " + path;
+
+                // Bắt đầu Timer
+                systemMetricsTimer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Thư mục không tồn tại!");
+            }
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (watcher != null)
+            {
+                watcher.EnableRaisingEvents = false;
+                watcher.Dispose();
+                lblStatus.Text = "Đã dừng giám sát.";
+                lblStatus.Text = "Đã dừng giám sát.";
+            }
+
+            // Dừng Timer
+            systemMetricsTimer.Stop();
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+            // nếu có dữ liệu trong listBoxLogs thì mới hiện thông báo
+            if (listBoxLogs.Items.Count > 0)
+            {
+                // Alert người dùng có muốn xuất ra file ko?
+                DialogResult dialogResult = MessageBox.Show(
+                    "Bạn có muốn xuất ra file log.txt không?",
+                    "Xuất file log",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    // Hộp thoại chọn đường dẫn lưu file
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                        saveFileDialog.Title = "Chọn nơi lưu file log";
+                        saveFileDialog.FileName = "log.txt";
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            try
+                            {
+                                // Tạo nội dung log từ listBoxLogs
+                                string logContent = string.Join(Environment.NewLine, listBoxLogs.Items.Cast<string>());
+
+                                // Ghi dữ liệu vào file
+                                File.WriteAllText(saveFileDialog.FileName, logContent);
+
+                                MessageBox.Show(
+                                    "File log đã được xuất thành công!",
+                                    "Thông báo",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(
+                                    $"Đã xảy ra lỗi khi xuất file log:\n{ex.Message}",
+                                    "Lỗi",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Xóa dữ liệu trong listBoxLogs
+            listBoxLogs.Items.Clear();
+        }
+
+        // on open file dialog
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Chọn thư mục cần giám sát";
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtDirectory.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        // làm việc với file
+        private bool IsCriticalFile(string filePath)
+        {
+            // Danh sách các tệp quan trọng cần giám sát
+            string[] criticalFiles = { "C:\\path\\to\\important_file.txt" };
+            return criticalFiles.Contains(filePath);
+        }
+
+        // gửi cảnh báo qua email của quản lý
+        private void SendAlert(string message)
+        {
+            // Gửi cảnh báo qua email
+            MessageBox.Show(message, "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            string logMessage = $"Tệp {e.ChangeType}: {e.FullPath}";
+            Invoke((MethodInvoker)delegate
+            {
+                listBoxLogs.Items.Add(logMessage);
+            });
+            WriteLog(logMessage);
+
+            // Kiểm tra hành vi bất thường
+            if (e.ChangeType == WatcherChangeTypes.Deleted && IsCriticalFile(e.FullPath))
+            {
+                SendAlert($"Cảnh báo: Tệp quan trọng đã bị xóa: {e.FullPath}");
+            }
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            string logMessage = $"Tệp đổi tên: {e.OldFullPath} thành {e.FullPath}";
+            Invoke((MethodInvoker)delegate
+            {
+                listBoxLogs.Items.Add(logMessage);
+            });
+            WriteLog(logMessage);
+        }
+
+        private void WriteLog(string message)
+        {
+            string logFilePath = "log.txt"; // Đường dẫn tệp nhật ký
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+
+        private void SystemMetricsTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateSystemMetrics();
+        }
+
+        private void UpdateSystemMetrics()
+        {
+            float cpuUsage = cpuCounter.NextValue();
+            float availableRam = ramCounter.NextValue();
+
+            Invoke((MethodInvoker)delegate
+            {
+                lblCpuUsage.Text = $"CPU Usage: {cpuUsage}%";
+                lblAvailableRam.Text = $"Available RAM: {availableRam} MB";
+            });
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
